c75b2a414ffbdefa3a13dbb689d54a8621dd173a
Krischan Stapelfeldt <Krischan@Laptop>
2014-06-26 11:26:32 +0200

    TestSuite-Name edit

--- src/tests/AllTests.java
+++ src/tests/AllTests.java
@@ -5,7 +5,7 @@ import junit.framework.TestSuite;
 
 public class AllTests extends TestSuite {
 	public static Test suite() {
-		TestSuite mySuite = new TestSuite("Meine Test-Suite");
+		TestSuite mySuite = new TestSuite("Global Poseidon TestSuite");
 		mySuite.addTestSuite(tests.ApartmentTest.class);
 		mySuite.addTestSuite(tests.BuildingTest.class);
 		mySuite.addTestSuite(tests.EntranceTest.class);
