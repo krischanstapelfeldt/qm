5e5e0a4357a621eb089ed7d41808e7f1ef4deea6
Sebastian Lauber <sebastian.lauber@dasscrumteam.com>
2014-08-21 18:18:49 +0200

    Introducing Developermenu - Links to Jenkins, Mantis, Testlink

--- src/de/globalposeidon/Qualitaet/gui/MainMenuBar.java
+++ src/de/globalposeidon/Qualitaet/gui/MainMenuBar.java
@@ -1,7 +1,12 @@
 package de.globalposeidon.Qualitaet.gui;
 
+import java.awt.Desktop;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -12,6 +17,25 @@ import de.globalposeidon.Qualitaet.Strings;
 public class MainMenuBar extends JMenuBar{
 	private static final long serialVersionUID = 3409407521701819128L;
 
+	private void openWebpage(URI uri) {
+	    Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+	    if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+	        try {
+	            desktop.browse(uri);
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	    }
+	}
+	
+	private void openWebpage(String urlString){
+		try {
+			openWebpage(new URI(urlString));
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public MainMenuBar(){		
 		JMenu file = new JMenu(Strings.file);
 		add(file);
@@ -29,14 +53,47 @@ public class MainMenuBar extends JMenuBar{
 		JMenu edit = new JMenu(Strings.edit);
 		add(edit);
 		
-		JMenuItem building = new JMenuItem(Strings.addBuilding);
-		edit.add(building);
-		
 		JMenuItem apartment = new JMenuItem(Strings.addAppartment);
 		edit.add(apartment);
 		
+		JMenuItem building = new JMenuItem(Strings.addBuilding);
+		edit.add(building);
+		
 		JMenuItem renter = new JMenuItem(Strings.addRenter);
 		edit.add(renter);
+		
+		JMenu dev = new JMenu("Developer");
+		add(dev);
+		
+		JMenuItem jenkins = new JMenuItem("open Jenkins");
+		jenkins.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				openWebpage("http://stapelfeldt.selfhost.eu:8080");
+			}
+		});
+		dev.add(jenkins);
+		
+		JMenuItem mantis = new JMenuItem("open Mantis");
+		mantis.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				openWebpage("http://stapelfeldt.selfhost.eu/mantis/mantisbt-1.2.17/");
+			}
+		});
+		dev.add(mantis);
+		
+		JMenuItem testlink = new JMenuItem("open Testlink");
+		testlink.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				openWebpage("http://stapelfeldt.selfhost.eu/testlink/testlink-1.9.10/");
+			}
+		});
+		dev.add(testlink);
 			
 		JMenu help = new JMenu(Strings.help);
 		add(help);
