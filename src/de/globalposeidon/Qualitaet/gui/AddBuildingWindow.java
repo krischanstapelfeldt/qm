6c0239cfe4a357ce3a4ad3506c87ed18322680e6
Timm <Timm@HadschiiDesk>
2014-08-30 09:47:17 +0200

    fixed addbuildingwindow and addtenantwindow

--- src/de/globalposeidon/Qualitaet/gui/AddBuildingWindow.java
+++ src/de/globalposeidon/Qualitaet/gui/AddBuildingWindow.java
@@ -2,6 +2,7 @@ package de.globalposeidon.Qualitaet.gui;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -9,10 +10,14 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
 
 import de.globalposeidon.Qualitaet.Strings;
 import de.globalposeidon.Qualitaet.model.Building;
 import de.globalposeidon.Qualitaet.model.DataContainer;
+import de.globalposeidon.Qualitaet.model.Renter;
+import de.globalposeidon.Qualitaet.model.Tenant;
 
 /**
  * AddBuildingWindow shows the user a window to either add a building with "OK" or to cancel the request with "Cancel".
@@ -21,15 +26,30 @@ import de.globalposeidon.Qualitaet.model.DataContainer;
 public class AddBuildingWindow extends JDialog {
 
    private static final long serialVersionUID = 4298418116709775705L;
-
+   
+   private final JTextField tfFirstname;
+   private final JTextField tfSurname;
+   private final JTextField tfTel;
+   private final JTextField tfEmail;
+   
    public AddBuildingWindow(final DataContainer model) {
 
       setTitle("add building");
 
       // ui components
-      final JPanel contentPnl = new JPanel();
-      final JLabel descriptionLbl = new JLabel("This will add a new building with a random ID to the tree");
-      contentPnl.add(descriptionLbl);
+      final JPanel contentPnl = new JPanel(new BorderLayout());
+      final JPanel tfPnl = new JPanel(new GridLayout(2, 3, 2, 2));
+      final JLabel descriptionLbl = new JLabel("This will add a new building with a random ID and a given Renter to the tree");
+      final JLabel lblFirstname = new JLabel("Firstname"); tfFirstname = new JTextField("");
+      final JLabel lblSurname = new JLabel("Surname");tfSurname = new JTextField("");
+      final JLabel lblTel = new JLabel("Phone");tfTel = new JTextField("");
+      final JLabel lblEmail = new JLabel("Mail");tfEmail = new JTextField("");
+      tfPnl.add(lblFirstname); tfPnl.add(tfFirstname);
+      tfPnl.add(lblSurname); tfPnl.add(tfSurname);
+      tfPnl.add(lblTel); tfPnl.add(tfTel);
+      tfPnl.add(lblEmail); tfPnl.add(tfEmail);
+      contentPnl.add(descriptionLbl, BorderLayout.NORTH);
+      contentPnl.add(tfPnl, BorderLayout.CENTER);
 
       final JPanel buttonPnl = new JPanel(new FlowLayout());
       final JButton saveBtn = new JButton(Strings.OK);
@@ -64,8 +84,21 @@ public class AddBuildingWindow extends JDialog {
    }
 
    private void addBuilding(final DataContainer container) {
-
-      container.addBuilding(new Building(container));
-      dispose();
+	  
+	   if (tfFirstname.getText().equals("")) {
+		   tfFirstname.requestFocus();
+	   } else if (tfSurname.getText().equals("")) {
+		   tfSurname.requestFocus();
+	   } else if (tfTel.getText().equals("")) {
+		   tfTel.requestFocus();
+	   } else if (tfEmail.getText().equals("")) {
+		   tfEmail.requestFocus();
+	   } else { // all fields valid
+		   Renter tmpRenter = new Renter(tfFirstname.getText(), tfSurname.getText(), tfTel.getText(), tfEmail.getText());
+		   Building tmpBuilding = new Building(container);
+		   tmpBuilding.setRenter(tmpRenter);
+		   container.addBuilding(tmpBuilding);
+		   dispose();
+	   }
    }
 }
--- src/de/globalposeidon/Qualitaet/gui/AddTenantWindow.java
+++ src/de/globalposeidon/Qualitaet/gui/AddTenantWindow.java
@@ -2,6 +2,7 @@ package de.globalposeidon.Qualitaet.gui;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -13,6 +14,8 @@ import javax.swing.JTextField;
 
 import de.globalposeidon.Qualitaet.Strings;
 import de.globalposeidon.Qualitaet.model.Apartment;
+import de.globalposeidon.Qualitaet.model.Building;
+import de.globalposeidon.Qualitaet.model.Renter;
 import de.globalposeidon.Qualitaet.model.Tenant;
 
 /**
@@ -35,17 +38,19 @@ public class AddTenantWindow extends JDialog {
       setTitle("add tenant");
 
       // ui components
-      final JPanel contentPnl = new JPanel(new FlowLayout());
+      final JPanel contentPnl = new JPanel(new BorderLayout());
+      final JPanel tfPnl = new JPanel(new GridLayout(2, 3, 2, 2));
       final JLabel descriptionLbl = new JLabel("This will add a new tenant to the apartment with ID: " + apartment.getID());
-      tfFirstname = new JTextField("Firstname");
-      tfSurname = new JTextField("Surname");
-      tfTel = new JTextField("Phone");
-      tfEmail = new JTextField("Mail");
-      contentPnl.add(descriptionLbl);
-      contentPnl.add(tfFirstname);
-      contentPnl.add(tfSurname);
-      contentPnl.add(tfTel);
-      contentPnl.add(tfEmail);
+      final JLabel lblFirstname = new JLabel("Firstname"); tfFirstname = new JTextField("");
+      final JLabel lblSurname = new JLabel("Surname");tfSurname = new JTextField("");
+      final JLabel lblTel = new JLabel("Phone");tfTel = new JTextField("");
+      final JLabel lblEmail = new JLabel("Mail");tfEmail = new JTextField("");
+      tfPnl.add(lblFirstname); tfPnl.add(tfFirstname);
+      tfPnl.add(lblSurname); tfPnl.add(tfSurname);
+      tfPnl.add(lblTel); tfPnl.add(tfTel);
+      tfPnl.add(lblEmail); tfPnl.add(tfEmail);
+      contentPnl.add(descriptionLbl, BorderLayout.NORTH);
+      contentPnl.add(tfPnl, BorderLayout.CENTER);
 
       final JPanel buttonPnl = new JPanel(new FlowLayout());
       final JButton saveBtn = new JButton(Strings.OK);
@@ -80,7 +85,18 @@ public class AddTenantWindow extends JDialog {
    }
 
    private void addTenant(final Apartment apartment) {
-      apartment.addTenant(new Tenant(tfFirstname.getText(), tfSurname.getText(), tfTel.getText(), tfEmail.getText()));
-      dispose();
+	   
+	   if (tfFirstname.getText().equals("")) {
+		   tfFirstname.requestFocus();
+	   } else if (tfSurname.getText().equals("")) {
+		   tfSurname.requestFocus();
+	   } else if (tfTel.getText().equals("")) {
+		   tfTel.requestFocus();
+	   } else if (tfEmail.getText().equals("")) {
+		   tfEmail.requestFocus();
+	   } else { // all fields valid
+		   apartment.addTenant(new Tenant(tfFirstname.getText(), tfSurname.getText(), tfTel.getText(), tfEmail.getText()));
+		   dispose();
+	   }
    }
 }
