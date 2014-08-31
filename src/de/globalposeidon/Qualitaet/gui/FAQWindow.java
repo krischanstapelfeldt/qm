8f91df4b2d8644e24f9b9e0772607aea470594e7
jrflieske <oddme5000@gmx.de>
2014-08-31 18:10:21 +0200

    Ok button in FAQ Hide Root Node

--- src/de/globalposeidon/Qualitaet/gui/FAQWindow.java
+++ src/de/globalposeidon/Qualitaet/gui/FAQWindow.java
@@ -1,7 +1,11 @@
 package de.globalposeidon.Qualitaet.gui;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 import org.slf4j.Logger;
@@ -15,12 +19,21 @@ public class FAQWindow extends JDialog {
       logger.debug("Add FAQ window opened");
       setTitle("faq");
       // add text
+      final JPanel contentPnl = new JPanel(new BorderLayout());
       final JTextArea textArea = new JTextArea("Hier eine FAQ schreiben...");
-      getContentPane().add(textArea);
+      
+      JButton btnOk = new JButton("Ok");
+      contentPnl.add(btnOk, BorderLayout.SOUTH);
+      contentPnl.add(textArea, BorderLayout.NORTH);
+      
+      this.add(contentPnl);
       // layout
       pack();
       setLocationRelativeTo(parent);
       setResizable(false);
       setVisible(true);
+      
+ 
+
    }
 }
